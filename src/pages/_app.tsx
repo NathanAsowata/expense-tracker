58f5f00051f502871005410adbf211a6cd7ee193
Nathan Asowata <nathanasowata@gmail.com>
2023-04-24 12:01:52 +0100

    feat: added navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import NavBar from '@/components/NavBar'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <NavBar />
+      <main className='main'>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
